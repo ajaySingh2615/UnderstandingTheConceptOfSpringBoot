fc5a844656e1071ca74b75f33bc3e4be0176f17c
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-06-03 00:55:11 +0530

    "spring data jpa"

--- src/main/java/com/ajay/Main.java
+++ src/main/java/com/ajay/Main.java
@@ -1,12 +1,30 @@
 package com.ajay;
 
+import com.ajay.customer.Customer;
+import com.ajay.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer mike = new Customer("mike", "mike@gmail.com", 23);
+            Customer eleven = new Customer("Eleven", "eleven@gmail.com", 21);
+
+            List<Customer> customers = List.of(mike, eleven);
+            customerRepository.saveAll(customers);
+        };
+
     }
 }
